e485081809ec113cb6b6255271417b14c7721e18
MOVRA <abgibun@gmail.com>
2026-06-22 21:52:00 +0700

    feat(ui): progress on translating design to code, finishing sign in form for now, modifiying auth side panel and sign in form width

--- src/common/AuthLeftSidePanel.tsx
+++ src/common/AuthLeftSidePanel.tsx
@@ -1,17 +1,15 @@
 export default function AuthLeftSidePanel() {
 	return (
-		<div className="h-screen w-[65%] border p-12 bg-linear-to-br from-zinc-950 via-indigo-900 to-indigo-700 flex flex-col justify-between items-start">
-			<h1 className="text-white text-xl font-bold font-['Plus_Jakarta_Sans']">
-				Invoix
-			</h1>
+		<div className="font-['Plus_Jakarta_Sans'] h-screen w-[64%] border p-12 bg-linear-to-br from-zinc-950 via-indigo-900 to-indigo-700 flex flex-col justify-between items-start">
+			<h1 className="text-white text-xl font-bold">Invoix</h1>
 
 			<div className="">
-				<h2 className="text-white text-4xl font-bold font-['Plus_Jakarta_Sans'] leading-10 mb-4">
+				<h2 className="text-white text-4xl font-bold leading-10 mb-4">
 					Invoice profesional
 					<br />
 					dalam hitungan menit.
 				</h2>
-				<p className="text-white/60 text-lg font-normal font-['Plus_Jakarta_Sans'] ">
+				<p className="text-white/60 text-lg font-normal ">
 					Platform manajemen invoice untuk freelancer Indonesia. Gratis
 					selamanya.
 				</p>
@@ -19,28 +17,28 @@ export default function AuthLeftSidePanel() {
 
 			<div className="flex gap-6">
 				<div className="flex-col">
-					<h2 className="justify-start text-white text-2xl font-bold font-['Plus_Jakarta_Sans']">
+					<h2 className="justify-start text-white text-2xl font-bold">
 						10K+
 					</h2>
-					<p className="justify-start text-white/50 text-sm font-normal font-['Plus_Jakarta_Sans']">
+					<p className="justify-start text-white/50 text-sm font-normal">
 						Freelancer
 					</p>
 				</div>
 
 				<div className="flex-col">
-					<h2 className="justify-start text-white text-2xl font-bold font-['Plus_Jakarta_Sans']">
+					<h2 className="justify-start text-white text-2xl font-bold">
 						50K+
 					</h2>
-					<p className="justify-start text-white/50 text-sm font-normal font-['Plus_Jakarta_Sans']">
+					<p className="justify-start text-white/50 text-sm font-normal">
 						Invoice dibuat
 					</p>
 				</div>
 
 				<div className="flex-col">
-					<h2 className="justify-start text-white text-2xl font-bold font-['Plus_Jakarta_Sans']">
+					<h2 className="justify-start text-white text-2xl font-bold">
 						98%
 					</h2>
-					<p className="justify-start text-white/50 text-sm font-normal font-['Plus_Jakarta_Sans']">
+					<p className="justify-start text-white/50 text-sm font-normal">
 						Klien puas
 					</p>
 				</div>
--- src/features/sign-in/pages-sign-in/SignIn.tsx
+++ src/features/sign-in/pages-sign-in/SignIn.tsx
@@ -6,17 +6,17 @@ import { Label } from "@/common/label/Label";
 
 export default function SignIn() {
 	return (
-		<div className="w-[35%] p-12 bg-white flex flex-col items-center justify-center h-screen">
+		<div className="w-[36%] p-12 bg-white flex flex-col items-center justify-center h-screen font-['Plus_Jakarta_Sans']">
 			<div className="mb-7 w-full">
-				<h2 className="text-zinc-950 text-2xl font-bold font-['Plus_Jakarta_Sans'] mb-1">
+				<h2 className="text-zinc-950 text-2xl font-bold mb-1">
 					Selamat datang kembali
 				</h2>
-				<p className="justify-start text-gray-500 text-sm font-normal font-['Plus_Jakarta_Sans'] leading-5">
+				<p className="justify-start text-gray-500 text-sm font-normal leading-5">
 					Masuk ke akun Invoix kamu
 				</p>
 			</div>
 
-			<form className="flex flex-col gap-4 w-full">
+			<form className="flex flex-col gap-4 w-full mb-6">
 				<div className="flex flex-col gap-1.5 w-full">
 					<Label htmlFor="email">Email</Label>
 					<Input id="email" placeholder="Masukkan email" />
@@ -27,7 +27,7 @@ export default function SignIn() {
 						<Label htmlFor="password">Password</Label>
 						<Link
 							to="/forgot-password"
-							className="text-violet-700 text-xs font-medium font-['Plus_Jakarta_Sans']"
+							className="text-violet-700 text-xs font-medium"
 						>
 							Lupa password?
 						</Link>
@@ -35,8 +35,40 @@ export default function SignIn() {
 					<InputPassword id="password" placeholder="Masukkan password" />
 				</div>
 
-				<Button>Masuk</Button>
+				<Button className="h-10">Masuk</Button>
 			</form>
+
+			<div className="flex justify-start w-full items-center gap-3 mb-6">
+				<div className="w-full h-px relative bg-zinc-950/8"></div>
+
+				<div className="relative w-12">
+					<div className="absolute -top-2 text-gray-500 text-xs font-normal">
+						atau
+					</div>
+				</div>
+
+				<div className="w-full h-px relative bg-zinc-950/8"></div>
+			</div>
+
+			<button
+				type="button"
+				className="cursor-pointer py-2.5 h-10 w-full mb-4 rounded-xl outline outline-offset-[-0.74px] outline-zinc-950/5 flex justify-center items-center gap-2.5"
+			>
+				<img src="/google.svg" />
+				<span className="text-zinc-950 text-sm font-medium">
+					Masuk dengan Google
+				</span>
+			</button>
+
+			<p className="text-gray-500 text-sm font-normal flex gap-1 items-center">
+				Belum punya akun?
+				<Link
+					to="/register"
+					className="text-violet-700 text-base font-semibold"
+				>
+					Daftar gratis
+				</Link>
+			</p>
 		</div>
 	);
 }
