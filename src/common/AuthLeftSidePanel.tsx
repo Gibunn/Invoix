782a87f3a4c8ab687c0122affcbb274dcf7a6195
MOVRA <abgibun@gmail.com>
2026-06-22 16:33:53 +0700

    feat(ui): translating design to code, auth side panel, and adding font to index.html

--- src/common/AuthLeftSidePanel.tsx
+++ src/common/AuthLeftSidePanel.tsx
@@ -1,3 +1,62 @@
 export default function AuthLeftSidePanel() {
-	return <div>AuthLeftSidePanel</div>;
+	return (
+		<div className="h-screen w-full border p-12 bg-linear-to-br from-zinc-950 via-indigo-900 to-indigo-700 flex flex-col justify-between items-start">
+			<h1 className="text-white text-xl font-bold font-['Plus_Jakarta_Sans']">
+				Invoix
+			</h1>
+
+			<div className="">
+				<h2 className="text-white text-4xl font-bold font-['Plus_Jakarta_Sans'] leading-10 mb-4">
+					Invoice profesional
+					<br />
+					dalam hitungan menit.
+				</h2>
+				<p className="text-white/60 text-lg font-normal font-['Plus_Jakarta_Sans'] ">
+					Platform manajemen invoice untuk freelancer Indonesia. Gratis
+					selamanya.
+				</p>
+			</div>
+
+			<div className="flex gap-6">
+				<div className="flex-col">
+					<div className="flex flex-col">
+						<div className="justify-start text-white text-2xl font-bold font-['Plus_Jakarta_Sans']">
+							10K+
+						</div>
+					</div>
+					<div className="flex flex-col">
+						<div className="justify-start text-white/50 text-sm font-normal font-['Plus_Jakarta_Sans']">
+							Freelancer
+						</div>
+					</div>
+				</div>
+
+				<div className="flex-col">
+					<div className="flex flex-col">
+						<div className="justify-start text-white text-2xl font-bold font-['Plus_Jakarta_Sans']">
+							50K+
+						</div>
+					</div>
+					<div className="flex flex-col">
+						<div className="justify-start text-white/50 text-sm font-normal font-['Plus_Jakarta_Sans']">
+							Invoice dibuat
+						</div>
+					</div>
+				</div>
+
+				<div className="flex-col">
+					<div className="flex flex-col">
+						<div className="justify-start text-white text-2xl font-bold font-['Plus_Jakarta_Sans']">
+							98%
+						</div>
+					</div>
+					<div className="flex flex-col">
+						<div className="justify-start text-white/50 text-sm font-normal font-['Plus_Jakarta_Sans']">
+							Klien puas
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
